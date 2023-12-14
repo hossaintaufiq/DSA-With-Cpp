017dffa11a7944536ae149c8d0b75a36806ef2cc
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2023-12-14 11:37:52 +0600

    starting newly

--- F. Diamond Theft.cpp
+++ F. Diamond Theft.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 
 int main (){
-    int t; 
-    cin>>t;
-    while(t!=0){
-        
+    int t;
+    cin>>t; 
+    while (t!=0)
+    {
+        int x; 
+        cin>>x;
 
+        
         t--;
     }
+    
 }
