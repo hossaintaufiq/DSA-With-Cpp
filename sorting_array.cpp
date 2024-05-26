1e15a83f8ba55dc3779a0eaf24f0f78461b9f5b4
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2024-05-27 00:12:52 +0600

    problem 3 done

--- sorting_array.cpp
+++ sorting_array.cpp
@@ -31,29 +31,70 @@
 
 
 
+// #include<bits/stdc++.h>
+// using namespace std; 
+
+// int *sort_it(int n){
+// 	int *a = new int[n];
+// 	for(int i=0;i<n;i++){
+// 		cin>>a[i];
+// 	}
+
+// 	sort(a,a+n,greater<int>());
+
+// 	return a;
+
+// }
+
+// int main(){
+// 	int n;
+// 	cin>>n; 
+
+// 	int *a = sort_it(n);
+
+	// for(int i=0;i<n; i++){
+	// 	cout<<a[i]<<" ";
+	// }
+
+// }
+
+
+
 #include<bits/stdc++.h>
 using namespace std; 
 
-int *sort_it(int n){
-	int *a = new int[n];
-	for(int i=0;i<n;i++){
-		cin>>a[i];
-	}
-
-	sort(a,a+n,greater<int>());
-
-	return a;
-
-}
-
 int main(){
-	int n;
-	cin>>n; 
+	string s;
+	
 
-	int *a = sort_it(n);
+	while(getline(cin,s)){
 
-	for(int i=0;i<n; i++){
-		cout<<a[i]<<" ";
+		int a[s.length()];
+		int l= s.length();
+
+		for(int i=0;i<s.length();i++){
+			int x= (int)s[i];
+			a[i]=x;
+			// cout<<s[i]<<" ";
+		}
+
+		sort(a,a+l);
+
+		for(int i=0;i<s.length(); i++){
+
+
+			char ch= (char)a[i];
+			if(ch==' '){
+				continue;
+			}
+			else{
+				cout<<ch;
+			}
+			
+
+			}
+
+		// cout<<s<<endl;
+			cout<<endl;
 	}
-
 }
