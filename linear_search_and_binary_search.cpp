01934ae224ced348f2c1af7de742605386741095
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2024-09-30 21:52:41 +0600

    binary and linear search

---
+++ linear_search_and_binary_search.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+ void linearSearch(int*a,int n, int val){
+
+ 	bool result =false ;
+ 	int count; 
+	for(int i=0;i<n;i++){
+		if(a[i]==val) {
+			result=true;
+			count=i; 
+			break;
+		}
+		
+	}
+
+	if(result){
+		cout<<"Found at index : "<<count ; 
+	}
+	else {
+		cout<<"Not found";
+	}
+
+
+	
+}
+
+int main(){
+	cout<<"<<<--------------Linear search---------------->>> "<<endl<<endl;
+	int n; 
+	cin>>n; 
+	int *array = new int [n];
+
+	for(int i=0;i<n; i++){
+		cin>>array[i];
+	}
+
+	
+
+	int value;
+	cin>>value;
+ 	linearSearch(array, n, value);
+ 	cout<<endl;
+ 	cout<<endl;
+
+ 	
+
+
+ 	// sort(first, last );
+
+
+ 	for(int i=0;i<n;i++){
+		cout<<array[i]<<" ";
+	}
+
+
+	return 0; 
+
+}
